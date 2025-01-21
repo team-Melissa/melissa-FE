4a62e42d1acdf52b1ed351a2e47e366bf03c8137
NARARIA03 <hyeonseong0305@gmail.com>
2025-01-22 06:44:06 +0900

    KAN-90 Refactor: 접속 초기에 실행되는 함수들 모듈화
    initializeApp.ts 에는 여러 함수들과(접속 초기에 수행해야 하는 비동기
    처리들을 하나씩 담당할 예정), 모든 함수를 Promise.all로 병렬 실행하고
    state를 변경하고 스플래시 스크린을 끄는 initializeApp 함수가 존재.
    
    앞으로 추가될 코드는 자동 로그인 등이 있을 것으로 생각됨.
    
    해당 코드를 개발할 때는 비동기 함수를 만들고 Promise.all에 추가하면 되고,
    해당 코드를 사용할 때는 빈 배열을 dependency로 가지는 useEffect에서 호출
    만 하면 되도록 설계했음

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,10 @@
 import * as SplashScreen from "expo-splash-screen";
-import * as Font from "expo-font";
 import { Stack } from "expo-router";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "@/src/constants/theme";
 import { useEffect, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import initializeApp from "@/src/utils/initializeApp";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -14,24 +14,7 @@ export default function RootLayout() {
   const [isReady, setIsReady] = useState<boolean>(false);
 
   useEffect(() => {
-    const prepare = async () => {
-      try {
-        await Font.loadAsync({
-          nanumSquareNeoLight: require("@/assets/fonts/NanumSquareNeo-light.ttf"),
-          nanumSquareNeoRegular: require("@/assets/fonts/NanumSquareNeo-regular.ttf"),
-          nanumSquareNeoBold: require("@/assets/fonts/NanumSquareNeo-bold.ttf"),
-          nanumSquareNeoExtraBold: require("@/assets/fonts/NanumSquareNeo-extrabold.ttf"),
-          nanumSquareNeoHeavy: require("@/assets/fonts/NanumSquareNeo-heavy.ttf"),
-        });
-      } catch (e) {
-        console.error(e);
-      } finally {
-        setIsReady(true);
-        SplashScreen.hideAsync();
-      }
-    };
-
-    prepare();
+    initializeApp(setIsReady);
   }, []);
 
   if (!isReady) return null;
