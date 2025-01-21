ceb941b48c2d6adbc02db6fcd003ac77cc2249d1
NARARIA03 <hyeonseong0305@gmail.com>
2025-01-21 09:11:40 +0900

    KAN-90 Fix: 절대경로 src -> 루트로 되돌림

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,7 +2,7 @@ import * as SplashScreen from "expo-splash-screen";
 import * as Font from "expo-font";
 import { Stack } from "expo-router";
 import { ThemeProvider } from "styled-components/native";
-import { theme } from "@/styles/theme";
+import { theme } from "@/src/styles/theme";
 import { useEffect, useState } from "react";
 
 SplashScreen.preventAutoHideAsync();
@@ -14,11 +14,11 @@ export default function RootLayout() {
     const prepare = async () => {
       try {
         await Font.loadAsync({
-          nanumSquareNeoLight: require("../../assets/fonts/NanumSquareNeo-light.ttf"),
-          nanumSquareNeoRegular: require("../../assets/fonts/NanumSquareNeo-regular.ttf"),
-          nanumSquareNeoBold: require("../../assets/fonts/NanumSquareNeo-bold.ttf"),
-          nanumSquareNeoExtraBold: require("../../assets/fonts/NanumSquareNeo-extrabold.ttf"),
-          nanumSquareNeoHeavy: require("../../assets/fonts/NanumSquareNeo-heavy.ttf"),
+          nanumSquareNeoLight: require("@/assets/fonts/NanumSquareNeo-light.ttf"),
+          nanumSquareNeoRegular: require("@/assets/fonts/NanumSquareNeo-regular.ttf"),
+          nanumSquareNeoBold: require("@/assets/fonts/NanumSquareNeo-bold.ttf"),
+          nanumSquareNeoExtraBold: require("@/assets/fonts/NanumSquareNeo-extrabold.ttf"),
+          nanumSquareNeoHeavy: require("@/assets/fonts/NanumSquareNeo-heavy.ttf"),
         });
       } catch (e) {
         console.error(e);
