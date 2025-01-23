3e37ff9b5977ae2c9c3d620f293734c4091461d6
NARARIA03 <hyeonseong0305@gmail.com>
2025-01-23 09:04:12 +0900

    KAN-65 Refactor: initializeApp 로직을 안쪽 layout으로 이동
    기존 layout 구조에서는 2가지 문제가 있었음
    1. RootLayout에서 Provider로 감싸주는 동시에 ThemeProvider에 접근해야 했음
    2. Splash 컴포넌트가 한 번 깜빡일 수밖에 없음 (폰트 로딩 끝나면 언마운트되고, 토큰 검증으로 인해 다시 마운트)
    
    이를 해결하기 위해 RootLayout을 ProviderLayout으로 변경해 여기서는 Provider로만 감싸주고
    AuthLayout을 RootLayout으로 변경해 폰트 로딩과 토큰 검증을 동시에 수행하며 Splash 컴포넌트를 깜빡이지 않게 수정

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,27 +1,16 @@
-import { useEffect, useState } from "react";
 import * as SplashScreen from "expo-splash-screen";
 import { Slot } from "expo-router";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "@/src/constants/theme";
 import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "@/src/libs/queryClient";
-import initializeApp from "@/src/utils/initializeApp";
-import Splash from "@/src/components/ui/Splash";
 
 SplashScreen.preventAutoHideAsync();
 
 /**
- * @description 모든 초기 로직을 실행시키고 Provider로 감싸주는 레이아웃
+ * @description 필요한 Provider들을 제공하는 레이아웃
  */
-export default function RootLayout() {
-  const [isReady, setIsReady] = useState<boolean>(false);
-
-  useEffect(() => {
-    initializeApp(setIsReady);
-  }, []);
-
-  if (!isReady) return <Splash />;
-
+function ProviderLayout() {
   return (
     <ThemeProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
@@ -30,3 +19,5 @@ export default function RootLayout() {
     </ThemeProvider>
   );
 }
+
+export default ProviderLayout;
--- src/components/ui/Splash/index.tsx
+++ src/components/ui/Splash/index.tsx
@@ -1,15 +1,28 @@
-import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Image as Img } from "expo-image";
+import styled from "styled-components/native";
 
 /**
  * @description SplashScreen이 닫혀도 필요한 시간동안 더 띄워두기 위한 컴포넌트
  */
 function Splash(): JSX.Element {
   return (
-    <SafeAreaView>
-      <Text>Loading... Splash</Text>
-    </SafeAreaView>
+    <Layout>
+      <Image source={require("@/assets/images/splash-icon.png")} />
+    </Layout>
   );
 }
+
+const Layout = styled(SafeAreaView)`
+  background-color: "#fff";
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Image = styled(Img)`
+  width: 200px;
+  height: 200px;
+`;
 
 export default Splash;
