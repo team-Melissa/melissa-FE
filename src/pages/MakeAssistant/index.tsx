792dfb1a8fce91a8d649ee6b3c4622d44e1e7eb6
NARARIA03 <hyeonseong0305@gmail.com>
2025-01-24 06:33:32 +0900

    KAN-65 Feat: 질문 페이지 Q2 레이아웃/애니메이션 구현

--- src/pages/MakeAssistant/index.tsx
+++ src/pages/MakeAssistant/index.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
+import Button from "@/src/components/ui/Button";
+import question from "@/src/constants/question";
 import * as S from "./styles";
+
+import { FadeIn, FadeOut } from "react-native-reanimated";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { Text } from "react-native";
 
 function MakeAssistantPage() {
   return (
     <S.SafeLayout>
-      <Start />
+      <Question />
     </S.SafeLayout>
   );
 }
@@ -18,6 +25,52 @@ function Start() {
         당신에게 맞는 <S.Bold>서포터</S.Bold>를 만들어드릴게요
       </S.InfoText>
     </S.CenteringBox>
+  );
+}
+
+function Question() {
+  const [cursor, setCursor] = useState<number>(0);
+
+  const handleNextBtn = () => {
+    if (cursor < question.length - 1) setCursor(cursor + 1);
+  };
+
+  const handlePrevBtn = () => {
+    if (cursor > 0) setCursor(cursor - 1);
+  };
+
+  return (
+    <S.BetweenBox>
+      <S.HeaderBox>
+        {/* Todo: 진행상황 바 만들기, 버튼 클릭 시 색 변하게 하기*/}
+        <Text>
+          {cursor + 1} / {question.length}
+        </Text>
+        <S.ProgressBarBox>
+          <AntDesign name="left" size={24} color="black" onPress={handlePrevBtn} />
+
+          <AntDesign name="right" size={24} color="black" onPress={handleNextBtn} />
+        </S.ProgressBarBox>
+      </S.HeaderBox>
+
+      <S.AnimatedBodyBox
+        entering={FadeIn.delay(900).duration(300)}
+        exiting={FadeOut.duration(300)}
+        key={cursor} // cursor가 바뀔 때마다 새로운 뷰로 인식
+      >
+        <S.QuestionBox>
+          <S.QuestionText>{question[cursor].q}</S.QuestionText>
+        </S.QuestionBox>
+
+        <S.ButtonBox>
+          {question[cursor].a.map((e, i) => (
+            <Button key={e} color="white" textColor="black">
+              {e}
+            </Button>
+          ))}
+        </S.ButtonBox>
+      </S.AnimatedBodyBox>
+    </S.BetweenBox>
   );
 }
 
--- src/pages/MakeAssistant/styles.ts
+++ src/pages/MakeAssistant/styles.ts
@@ -1,3 +1,4 @@
+import Animated from "react-native-reanimated";
 import { SafeAreaView } from "react-native-safe-area-context";
 import styled from "styled-components/native";
 
@@ -6,7 +7,7 @@ export const SafeLayout = styled(SafeAreaView)`
 `;
 
 export const CenteringBox = styled.View`
-  height: 100%;
+  flex: 1;
   justify-content: center;
   align-items: center;
 `;
@@ -21,8 +22,51 @@ export const Bold = styled(InfoText)`
   font-family: ${({ theme }) => theme.fontFamily.nsBold};
 `;
 
+// ----------------------- Question ---------------------------------
+
+export const BetweenBox = styled.View`
+  flex: 1;
+  padding: 0px 0px 120px 0px;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const HeaderBox = styled.View`
+  width: 100%;
+  height: 20%;
+
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ProgressBarBox = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const AnimatedBodyBox = styled(Animated.View)`
+  width: 100%;
+  flex: 1;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const QuestionBox = styled.View`
+  width: 80%;
+`;
+
+export const ButtonBox = styled.View`
+  width: 100%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 24px;
+`;
+
 export const QuestionText = styled.Text`
+  align-self: baseline;
   font-family: ${({ theme }) => theme.fontFamily.nsBold};
   font-size: ${({ theme }) => theme.fontSize.title};
+  line-height: 40px;
   padding-bottom: 16px;
 `;
