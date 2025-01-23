c4d254af959e93ed972a90f2cc9e59f6f3db0758
NARARIA03 <hyeonseong0305@gmail.com>
2025-01-23 09:51:41 +0900

    KAN-65 Feat: 스플래시스크린 컴포넌트 완성
    추후 로고와 배경만 변경해주면 된다

--- src/components/ui/Splash/index.tsx
+++ src/components/ui/Splash/index.tsx
@@ -1,28 +1,14 @@
-import { SafeAreaView } from "react-native-safe-area-context";
-import { Image as Img } from "expo-image";
-import styled from "styled-components/native";
+import * as S from "./styles";
 
 /**
  * @description SplashScreen이 닫혀도 필요한 시간동안 더 띄워두기 위한 컴포넌트
  */
 function Splash(): JSX.Element {
   return (
-    <Layout>
-      <Image source={require("@/assets/images/splash-icon.png")} />
-    </Layout>
+    <S.SafeLayout>
+      <S.LogoImage source={require("@/assets/images/splash-icon.png")} />
+    </S.SafeLayout>
   );
 }
-
-const Layout = styled(SafeAreaView)`
-  background-color: "#fff";
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Image = styled(Img)`
-  width: 200px;
-  height: 200px;
-`;
 
 export default Splash;
---
+++ src/components/ui/Splash/styles.ts
@@ -0,0 +1,15 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Image as Img } from "expo-image";
+import styled from "styled-components/native";
+
+export const SafeLayout = styled(SafeAreaView)`
+  background-color: ${({ theme }) => theme.colors.white};
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const LogoImage = styled(Img)`
+  width: 200px;
+  height: 200px;
+`;
