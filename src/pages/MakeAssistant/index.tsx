1dc1d7806fab0f964a7c0f613ef94982ecbc926e
NARARIA03 <hyeonseong0305@gmail.com>
2025-01-24 10:42:08 +0900

    KAN-65 Feat: 답변 저장, 이전 답변 없이 다음 질문 못 넘어가게 구현
    질문에 대한 답변이 순서대로 기록되어있는 상태인 answers와
    현재 사용자가 보고 있는 질문 번호인 cursor를 비교해
    cursor가 answer.length보다 작을 때만 다음 페이지로 이동 가능하도록 구현
    
    이전 답변 교체는 cursor와 index가 같은 값을 새 값으로 대체하는
    map함수를 활용해 구현 (splice메서드는 삭제와 삽입은 가능하나, 반환값이 삭
    제된 원소들이라 사용하려면 코드가 더 길어짐)

--- src/pages/MakeAssistant/index.tsx
+++ src/pages/MakeAssistant/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import Button from "@/src/components/ui/Button";
 import question from "@/src/constants/question";
 import * as S from "./styles";
@@ -14,6 +14,7 @@ import AntDesign from "@expo/vector-icons/AntDesign";
 
 function MakeAssistantPage() {
   const [isStart, setIsStart] = useState<boolean>(true);
+  const [answers, setAnswers] = useState<string[]>([]);
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -22,71 +23,126 @@ function MakeAssistantPage() {
 
     return () => clearTimeout(timer);
   }, []);
-  return <S.SafeLayout>{isStart ? <Start /> : <Question />}</S.SafeLayout>;
+
+  useEffect(() => {
+    console.log(answers);
+  }, [answers]);
+
+  if (isStart) {
+    return <Start />;
+  }
+
+  if (answers.length < question.length) {
+    return <Question answer={answers} setAnswer={setAnswers} />;
+  }
+
+  return null;
 }
 
 function Start() {
   return (
-    <S.CenteringBox>
-      <Animated.View entering={FadeIn.duration(1000)} exiting={FadeOut.duration(300)}>
-        <S.InfoText>
-          몇 가지 <S.Bold>질문</S.Bold>에 답변해주세요
-        </S.InfoText>
-      </Animated.View>
+    <S.SafeLayout>
+      <S.CenteringBox>
+        <Animated.View entering={FadeIn.duration(1000)} exiting={FadeOut.duration(300)}>
+          <S.InfoText>
+            몇 가지 <S.Bold>질문</S.Bold>에 답변해주세요
+          </S.InfoText>
+        </Animated.View>
 
-      <Animated.View entering={FadeIn.delay(1500).duration(1000)} exiting={FadeOut.duration(300)}>
-        <S.InfoText>
-          당신에게 맞는 <S.Bold>서포터</S.Bold>를 만들어드릴게요
-        </S.InfoText>
-      </Animated.View>
-    </S.CenteringBox>
+        <Animated.View entering={FadeIn.delay(1500).duration(1000)} exiting={FadeOut.duration(300)}>
+          <S.InfoText>
+            당신에게 맞는 <S.Bold>서포터</S.Bold>를 만들어드릴게요
+          </S.InfoText>
+        </Animated.View>
+      </S.CenteringBox>
+    </S.SafeLayout>
   );
 }
 
-function Question() {
+function Question({
+  answer,
+  setAnswer,
+}: {
+  answer: string[];
+  setAnswer: Dispatch<SetStateAction<string[]>>;
+}) {
   const [cursor, setCursor] = useState<number>(0);
 
   const handleNextBtn = () => {
-    if (cursor < question.length - 1) setCursor(cursor + 1);
+    // 답변하지 않고 다음 페이지로 못 넘어가게 조건 추가
+    if (cursor < question.length - 1 && cursor < answer.length) setCursor(cursor + 1);
   };
 
   const handlePrevBtn = () => {
     if (cursor > 0) setCursor(cursor - 1);
   };
 
+  const handleBtnPress = (text: string) => {
+    // 기존 answer 변경인지 새로 추가인지 조건 분기
+    if (cursor === answer.length) {
+      setAnswer((prev) => [...prev, text]);
+    } else {
+      // setAnswer((prev) => [...prev].splice(cursor, 1, text));
+      setAnswer((prev) => prev.map((e, i) => (i === cursor ? text : e)));
+    }
+
+    if (cursor < question.length - 1) setCursor(cursor + 1);
+  };
+
   return (
-    <S.BetweenBox>
-      <S.AnimatedHeaderBox entering={FadeIn.delay(900).duration(300)}>
-        {/* Todo: 진행 상황 기록하기, 체크하지 않으면 앞으로 못 가게 막기, 버튼 클릭 시 색 변하게 하기*/}
-        {/* Todo: 거대 컴포넌트들 쪼개기 */}
-        <S.HeaderText>
-          {cursor + 1}/{question.length}
-        </S.HeaderText>
-        <S.ProgressBarWrapper>
-          <AntDesign name="left" size={24} color="black" onPress={handlePrevBtn} />
-          <ProgressBar progress={(cursor + 1) / question.length} />
-          <AntDesign name="right" size={24} color="black" onPress={handleNextBtn} />
-        </S.ProgressBarWrapper>
-      </S.AnimatedHeaderBox>
+    <S.SafeLayout>
+      <S.BetweenBox>
+        <S.AnimatedHeaderBox
+          entering={FadeIn.delay(900).duration(300)}
+          exiting={FadeOut.duration(300)}
+        >
+          {/* Todo: 버튼 클릭 시 색 변하게 하기*/}
+          {/* Todo: 거대 컴포넌트들 쪼개기 */}
+          <S.HeaderText>
+            {cursor + 1}/{question.length}
+          </S.HeaderText>
+          <S.ProgressBarWrapper>
+            <AntDesign
+              name="left"
+              size={24}
+              color={cursor === 0 ? "gray" : "black"}
+              onPress={handlePrevBtn}
+            />
+            <ProgressBar progress={(cursor + 1) / question.length} />
+            <AntDesign
+              name="right"
+              size={24}
+              color={cursor === question.length - 1 || cursor >= answer.length ? "gray" : "black"}
+              onPress={handleNextBtn}
+            />
+          </S.ProgressBarWrapper>
+        </S.AnimatedHeaderBox>
 
-      <S.AnimatedBodyBox
-        entering={FadeIn.delay(900).duration(300)}
-        exiting={FadeOut.duration(300)}
-        key={cursor}
-      >
-        <S.QuestionBox>
-          <S.QuestionText>{question[cursor].q}</S.QuestionText>
-        </S.QuestionBox>
+        <S.AnimatedBodyBox
+          entering={FadeIn.delay(900).duration(300)}
+          exiting={FadeOut.duration(300)}
+          key={cursor}
+        >
+          <S.QuestionBox>
+            <S.QuestionText>{question[cursor].q}</S.QuestionText>
+          </S.QuestionBox>
 
-        <S.ButtonBox>
-          {question[cursor].a.map((e) => (
-            <Button key={e} color="white" textColor="black" fontFamily="nsRegular">
-              {e}
-            </Button>
-          ))}
-        </S.ButtonBox>
-      </S.AnimatedBodyBox>
-    </S.BetweenBox>
+          <S.ButtonBox>
+            {question[cursor].a.map((text) => (
+              <Button
+                key={text}
+                color="white"
+                textColor="black"
+                fontFamily="nsRegular"
+                onPress={() => handleBtnPress(text)}
+              >
+                {text}
+              </Button>
+            ))}
+          </S.ButtonBox>
+        </S.AnimatedBodyBox>
+      </S.BetweenBox>
+    </S.SafeLayout>
   );
 }
 
