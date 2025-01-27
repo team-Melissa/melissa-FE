9919c166e7788dc0a64ca66de00b1a1364a13584
NARARIA03 <hyeonseong0305@gmail.com>
2025-01-27 17:01:17 +0900

    MB1-66 Chore: expo secure store 설치 후 정상 빌드 확인

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -7,6 +7,7 @@ import LoginType from "../types/loginTypes";
 const useLogin = () => {
   const handleSuccess = (data: LoginType) => {
     console.log(`${data.result.oauthProvider} 로그인 성공!`);
+    // 이제 여기에 token type + access token 을 mmkv에 저장하고, refresh token을 secure storage에 저장하는 로직 추가
   };
   const handleError = (error: AxiosError<ErrorResponse>) => {
     console.error("로그인 실패!", error.response?.data);
