41057d05f4619c1c71fa70de02ccde8cfeb2684c
NARARIA03 <hyeonseong0305@gmail.com>
2025-01-27 14:58:28 +0900

    MB1-66 Chore: API 엔드포인트 상수화

---
+++ src/constants/endpoint.ts
@@ -0,0 +1,11 @@
+const endpoint = {
+  auth: {
+    kakao: "/api/v1/auth/kakao",
+    google: "/api/v1/auth/google",
+    apple: "/api/v1/auth/apple",
+    logout: "/api/v1/auth/logout",
+    refresh: "/api/v1/auth/refresh",
+  },
+};
+
+export default endpoint;
