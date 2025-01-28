ad00678be86935e1f9926a7269d416bdd72f8aab
NARARIA03 <hyeonseong0305@gmail.com>
2025-01-28 19:59:00 +0900

    MB1-130 Feat: 토큰 검증 useQuery를 RootLayout에 구현

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -1,19 +1,22 @@
 import { Redirect, Slot } from "expo-router";
 import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import Splash from "@/src/components/ui/Splash";
 import initializeApp from "@/src/utils/initializeApp";
+import { checkNewUserFn } from "@/src/apis/loginApi";
 
 /**
  * @description 앱 초기 로딩과 토큰 검증을 수행해 LoginPage와 IndexPage 중 하나로 이동시키는 레이아웃
  */
 function RootLayout() {
   const [isReady, setIsReady] = useState<boolean>(false);
-  // 토큰이 존재하는지, 존재한다면 유효한지, 유효하지 않다면 refresh token으로 재발급 가능한지 여기서 체크
-  // 체크하는동안 로딩 상태로 놔둠, 스플래시스크린 보여주면 될듯
-  // 토큰 검증 성공하면 스택 네비게이터 렌더링해서 index 그리기, 토큰 검증 실패하면 로그인으로 리다이렉트
-  // 아래 두 목업 변수는 토큰 검증 응답 값이 될 것
-  const isLoading = false;
-  const isAuthorized = false;
+  // 토큰이 존재하는지, 존재한다면 유효한지 검증
+  // 체크하는동안 로딩 상태로 스플래시스크린 렌더링
+  // 토큰 검증 성공하면 IndexRouter로 이동, 토큰 검증 실패하면 로그인으로 리다이렉트
+  const { isLoading, isError } = useQuery({
+    queryFn: checkNewUserFn,
+    queryKey: ["check-new-user"],
+  });
 
   useEffect(() => {
     initializeApp(setIsReady);
@@ -23,7 +26,7 @@ function RootLayout() {
     return <Splash />;
   }
 
-  if (!isAuthorized) {
+  if (isError) {
     return <Redirect href="/login" />;
   }
 
