3629b7339d27181fa9cd1affbb504d2259fbc487
NARARIA03 <hyeonseong0305@gmail.com>
2025-01-28 19:55:28 +0900

    MB1-130 Feat: 토큰 검증 & 초기 유저 확인 queryFn 구현

--- src/apis/loginApi.ts
+++ src/apis/loginApi.ts
@@ -1,8 +1,9 @@
 import { login } from "@react-native-seoul/kakao-login";
 import { GoogleSignin } from "@react-native-google-signin/google-signin";
-import LoginType from "@/src/types/loginTypes";
 import axiosInstance from "../libs/axiosInstance";
 import endpoint from "../constants/endpoint";
+import LoginType from "@/src/types/loginTypes";
+import { CheckNewUser } from "@/src/types/settingTypes";
 
 GoogleSignin.configure({
   webClientId: process.env.EXPO_PUBLIC_WEB_CLIENT_ID,
@@ -20,5 +21,11 @@ export const googleLoginFn = async () => {
 export const kakaoLoginFn = async () => {
   const { accessToken } = await login();
   const { data } = await axiosInstance.post<LoginType>(endpoint.auth.kakao, { accessToken });
+  return data;
+};
+
+export const checkNewUserFn = async () => {
+  const { data } = await axiosInstance.get<CheckNewUser>(endpoint.setting.checkNew);
+  console.log(data);
   return data;
 };
