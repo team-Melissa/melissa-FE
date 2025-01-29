f5c850a21f25a8ac219e657c25ccf0cea07695ba
NARARIA03 <hyeonseong0305@gmail.com>
2025-01-29 19:00:01 +0900

    MB1-130 Chore: access token만 지우는 테스트 버튼 추가

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -19,7 +19,8 @@ function ProviderLayout() {
     <ThemeProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
         <SafeLayout>
-          <TestBtn />
+          <RemoveAllTokenBtn />
+          <RemoveAccessTokenBtn />
           <Slot />
         </SafeLayout>
       </QueryClientProvider>
@@ -31,7 +32,7 @@ const SafeLayout = styled(SafeAreaView)`
   flex: 1;
 `;
 
-const TestBtn = () => {
+const RemoveAllTokenBtn = () => {
   const removeTokens = async () => {
     removeStorageValue("accessToken");
     await removeSecureValue("refreshToken");
@@ -39,8 +40,20 @@ const TestBtn = () => {
 
   return (
     <FloatingButton onPress={removeTokens}>
-      <Text>토큰 삭제 버튼</Text>
+      <Text>토큰 전체 삭제 버튼</Text>
     </FloatingButton>
+  );
+};
+
+const RemoveAccessTokenBtn = () => {
+  const removeTokens = () => {
+    removeStorageValue("accessToken");
+  };
+
+  return (
+    <FloatingRightButton onPress={removeTokens}>
+      <Text>엑세스 토큰 삭제 버튼</Text>
+    </FloatingRightButton>
   );
 };
 
@@ -52,6 +65,10 @@ const FloatingButton = styled.TouchableOpacity`
   position: absolute;
   top: 20px;
   left: 20px;
+`;
+
+const FloatingRightButton = styled(FloatingButton)`
+  top: 60px;
 `;
 
 export default ProviderLayout;
