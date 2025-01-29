73a8cf4835b4b66825b3b7157009efbdce4ab206
NARARIA03 <hyeonseong0305@gmail.com>
2025-01-29 21:16:35 +0900

    MB1-130 Fix: 폰트 로딩 ProviderLayout으로 옮기고, 테스트버튼 제거

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -1,15 +1,12 @@
 import { Redirect, Slot } from "expo-router";
-import { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
-import Splash from "@/src/components/ui/Splash";
-import initializeApp from "@/src/utils/initializeApp";
 import { checkNewUserFn } from "@/src/apis/loginApi";
+import Loading from "@/src/components/ui/Loading";
 
 /**
- * @description 앱 초기 로딩과 토큰 검증을 수행해 LoginPage와 IndexPage 중 하나로 이동시키는 레이아웃
+ * @description 토큰 검증을 수행해 LoginPage와 IndexPage 중 하나로 이동시키는 레이아웃
  */
 function RootLayout() {
-  const [isReady, setIsReady] = useState<boolean>(false);
   // 토큰이 존재하는지, 존재한다면 유효한지 검증
   // 체크하는동안 로딩 상태로 스플래시스크린 렌더링
   // 토큰 검증 성공하면 IndexRouter로 이동, 토큰 검증 실패하면 로그인으로 리다이렉트
@@ -18,12 +15,8 @@ function RootLayout() {
     queryKey: ["check-new-user"],
   });
 
-  useEffect(() => {
-    initializeApp(setIsReady);
-  }, []);
-
-  if (isPending || !isReady) {
-    return <Splash />;
+  if (isPending) {
+    return <Loading />;
   }
 
   if (isError) {
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,12 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
 import * as SplashScreen from "expo-splash-screen";
 import { Slot } from "expo-router";
 import styled, { ThemeProvider } from "styled-components/native";
-import { theme } from "@/src/constants/theme";
-import { QueryClientProvider } from "@tanstack/react-query";
-import queryClient from "@/src/libs/queryClient";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { removeSecureValue } from "../libs/secureStorage";
-import { removeStorageValue } from "../libs/mmkv";
-import { Text } from "react-native";
+import { theme } from "@/src/constants/theme";
+import queryClient from "@/src/libs/queryClient";
+import initializeApp from "../utils/initializeApp";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -15,12 +14,20 @@ SplashScreen.preventAutoHideAsync();
  * @description 필요한 Provider들을 제공하는 레이아웃
  */
 function ProviderLayout() {
+  const [isReady, setIsReady] = useState<boolean>(false);
+
+  useEffect(() => {
+    initializeApp(setIsReady);
+  }, []);
+
+  if (!isReady) {
+    return null;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
         <SafeLayout>
-          <RemoveAllTokenBtn />
-          <RemoveAccessTokenBtn />
           <Slot />
         </SafeLayout>
       </QueryClientProvider>
@@ -30,45 +37,6 @@ function ProviderLayout() {
 
 const SafeLayout = styled(SafeAreaView)`
   flex: 1;
-`;
-
-const RemoveAllTokenBtn = () => {
-  const removeTokens = async () => {
-    removeStorageValue("accessToken");
-    await removeSecureValue("refreshToken");
-  };
-
-  return (
-    <FloatingButton onPress={removeTokens}>
-      <Text>토큰 전체 삭제 버튼</Text>
-    </FloatingButton>
-  );
-};
-
-const RemoveAccessTokenBtn = () => {
-  const removeTokens = () => {
-    removeStorageValue("accessToken");
-  };
-
-  return (
-    <FloatingRightButton onPress={removeTokens}>
-      <Text>엑세스 토큰 삭제 버튼</Text>
-    </FloatingRightButton>
-  );
-};
-
-const FloatingButton = styled.TouchableOpacity`
-  width: 200px;
-  height: 100px;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
-  top: 20px;
-  left: 20px;
-`;
-
-const FloatingRightButton = styled(FloatingButton)`
-  top: 60px;
 `;
 
 export default ProviderLayout;
