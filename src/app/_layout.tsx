fda1c571c728e75f0b09d6502ae1c6f311d145cf
NARARIA03 <hyeonseong0305@gmail.com>
2025-01-31 22:03:13 +0900

    MB1-140 Chore: tanstack query dev plugin 설치

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,4 +1,5 @@
 import { QueryClientProvider } from "@tanstack/react-query";
+import { useReactQueryDevTools } from "@dev-plugins/react-query";
 import { useCallback, useEffect, useState } from "react";
 import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -16,8 +17,10 @@ SplashScreen.preventAutoHideAsync();
  * @description 필요한 Provider들을 제공하는 레이아웃
  */
 function ProviderLayout() {
+  useReactQueryDevTools(queryClient);
   const [isReady, setIsReady] = useState<boolean>(false);
   const pathname = usePathname();
+
   console.log(pathname);
 
   const onLayoutRootView = useCallback(() => {
