33e93dc1b72fc23d9248807a33b0afd7b2b8a649
NARARIA03 <hyeonseong0305@gmail.com>
2025-02-01 17:27:34 +0900

    MB1-143 Feat: Figma와 동일한 Ai 캐러셀 구현 (모듈화 X)

--- src/components/AssistantList/index.tsx
+++ src/components/AssistantList/index.tsx
@@ -1,9 +1,9 @@
-import { useSharedValue } from "react-native-reanimated";
 import * as S from "./styles";
 import { AiProfile } from "@/src/types/aiProfileTypes";
-import { Dimensions, Text, View } from "react-native";
+import { Dimensions } from "react-native";
 import { useState } from "react";
 import Carousel from "react-native-reanimated-carousel";
+import { shadowProps } from "@/src/constants/shadowProps";
 
 interface Props {
   isVisible: boolean;
@@ -23,34 +23,33 @@ const mockData: AiProfile[] = [
     createdAt: "2025-01-31T23:40:04.767461",
   },
   {
-    aiProfileId: 4,
-    profileName: "유쾌한1 다람쥐",
+    aiProfileId: 5,
+    profileName: "차분한 토끼",
     imageUrl:
-      "https://melissa-s3.s3.ap-northeast-2.amazonaws.com/ai-profile/847ab57f-27aa-4a8b-b3d9-df1d0fe637e7",
-    hashTag1: "활발한1대화",
-    hashTag2: "유머가1득",
-    feature1: "활발하1고 유쾌한 성격",
-    feature2: "가벼운 1대화 주제 선호",
-    feature3: "공감을 1잘해주는 스타일",
-    createdAt: "2025-01-31T23:40:04.767461",
+      "https://melissa-s3.s3.ap-northeast-2.amazonaws.com/ai-profile/a59d7969-6ea7-4d5c-b27c-9f2245abd29e",
+    hashTag1: "감성토크",
+    hashTag2: "마음챙김",
+    feature1: "이해심이 깊음",
+    feature2: "내면의 이야기를 잘 들어줌",
+    feature3: "유머러스한 표현으로 위로",
+    createdAt: "2025-02-01T16:25:06.884713",
   },
   {
-    aiProfileId: 5,
-    profileName: "유쾌한2 다람쥐",
+    aiProfileId: 7,
+    profileName: "차분한 부엉이",
     imageUrl:
-      "https://melissa-s3.s3.ap-northeast-2.amazonaws.com/ai-profile/847ab57f-27aa-4a8b-b3d9-df1d0fe637e7",
-    hashTag1: "활발한2대화",
-    hashTag2: "유머가2득",
-    feature1: "활발하2고 유쾌한 성격",
-    feature2: "가벼운 2대화 주제 선호",
-    feature3: "공감을 2잘해주는 스타일",
-    createdAt: "2025-01-31T23:40:04.767461",
+      "https://melissa-s3.s3.ap-northeast-2.amazonaws.com/ai-profile/6026e9d8-8a28-4055-8481-b6f1c7a5d143",
+    hashTag1: "깊이있는대화",
+    hashTag2: "이해심",
+    feature1: "차분하고 이해심이 많음",
+    feature2: "전문적인 주제에 대한 깊이 있는 대화",
+    feature3: "유머러스한 표현으로 스트레스 해소",
+    createdAt: "2025-02-01T16:30:44.108886",
   },
 ];
 
 function AssistantList({ isVisible }: Props): JSX.Element | null {
   const [width] = useState<number>(() => Dimensions.get("window").width);
-  const progress = useSharedValue<number>(0);
 
   if (isVisible) {
     return (
@@ -61,16 +60,48 @@ function AssistantList({ isVisible }: Props): JSX.Element | null {
           loop={false}
           mode="parallax"
           modeConfig={{
-            parallaxScrollingScale: 0.9,
-            parallaxScrollingOffset: 50,
+            parallaxScrollingScale: 1,
+            parallaxScrollingOffset: 90,
+            parallaxAdjacentItemScale: 0.83,
           }}
           pagingEnabled={true}
           snapEnabled={true}
-          onProgressChange={progress}
           renderItem={({ item }) => (
-            <View>
-              <Text>{item.profileName}</Text>
-            </View>
+            <S.ItemLayout>
+              <S.ItemBox>
+                <S.ImageBox>
+                  <S.Image source={item.imageUrl} contentFit="contain" />
+                </S.ImageBox>
+                <S.TitleTagBox>
+                  <S.Title>{item.profileName}</S.Title>
+                  <S.Tag>
+                    #{item.hashTag1} #{item.hashTag2}
+                  </S.Tag>
+                </S.TitleTagBox>
+                <S.PersonalityBox>
+                  <S.TextBox>
+                    <S.PersonalityTitle>성 격</S.PersonalityTitle>
+                    <S.PersonalityContent>{item.feature1}</S.PersonalityContent>
+                  </S.TextBox>
+                  <S.TextBox>
+                    <S.PersonalityTitle>성 격</S.PersonalityTitle>
+                    <S.PersonalityContent>{item.feature2}</S.PersonalityContent>
+                  </S.TextBox>
+                  <S.TextBox>
+                    <S.PersonalityTitle>성 격</S.PersonalityTitle>
+                    <S.PersonalityContent>{item.feature3}</S.PersonalityContent>
+                  </S.TextBox>
+                </S.PersonalityBox>
+                <S.TextBox>
+                  <S.CardButton style={shadowProps}>
+                    <S.ButtonText>복제하기</S.ButtonText>
+                  </S.CardButton>
+                  <S.CardButton style={shadowProps}>
+                    <S.ButtonText>삭제하기</S.ButtonText>
+                  </S.CardButton>
+                </S.TextBox>
+              </S.ItemBox>
+            </S.ItemLayout>
           )}
         />
       </S.AssistantListLayout>
--- src/components/AssistantList/styles.ts
+++ src/components/AssistantList/styles.ts
@@ -1,15 +1,99 @@
 import styled from "styled-components/native";
-import Carousel from "react-native-reanimated-carousel";
+import { Image as Img } from "expo-image";
 import responsiveToPx from "@/src/utils/responsiveToPx";
-import { AiProfile } from "@/src/types/aiProfileTypes";
 
 export const AssistantListLayout = styled.View`
   background-color: rgba(0, 0, 0, 0.8);
   flex: 1;
 `;
 
-export const CarouselBox = styled(Carousel<AiProfile>)`
+export const ItemLayout = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+`;
+
+export const ItemBox = styled.View`
+  width: ${responsiveToPx("324px")};
+  height: ${responsiveToPx("520px")};
+  background-color: ${({ theme }) => theme.colors.deepGreen};
+  border-radius: ${({ theme }) => theme.borderRadius.sm};
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+export const ImageBox = styled.View`
+  width: ${responsiveToPx("210px")};
+  height: ${responsiveToPx("210px")};
+  background-color: ${({ theme }) => theme.colors.whiteBlue};
+  border-radius: 9999px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Image = styled(Img)`
+  width: ${responsiveToPx("187px")};
+  height: ${responsiveToPx("187px")};
+  border-radius: 9999px;
+`;
+
+export const TitleTagBox = styled.View`
+  justify-content: center;
+  align-items: center;
+  gap: ${({ theme }) => theme.gap.base};
+`;
+
+export const PersonalityBox = styled.View`
+  width: ${responsiveToPx("274px")};
+  padding: ${responsiveToPx("20px")};
+  background-color: ${({ theme }) => theme.colors.whiteBlue};
+  border-radius: ${({ theme }) => theme.borderRadius.base};
+  justify-content: center;
+  gap: ${({ theme }) => theme.gap.md};
+`;
+
+export const TextBox = styled.View`
+  flex-direction: row;
+  gap: ${({ theme }) => theme.gap.lg};
+`;
+
+export const CardButton = styled.TouchableOpacity`
+  width: ${responsiveToPx("101px")};
+  height: ${responsiveToPx("29px")};
+  background-color: ${({ theme }) => theme.colors.skyBlue};
+  border-radius: ${({ theme }) => theme.borderRadius.base};
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fontFamily.nsExtraBold};
+  font-size: ${({ theme }) => theme.fontSize.xl};
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+export const Tag = styled.Text`
+  font-family: ${({ theme }) => theme.fontFamily.nsBold};
+  font-size: ${({ theme }) => theme.fontSize.xs};
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+export const PersonalityTitle = styled.Text`
+  display: inline;
+  font-family: ${({ theme }) => theme.fontFamily.nsBold};
+  font-size: ${({ theme }) => theme.fontSize.base};
+  color: ${({ theme }) => theme.colors.deepGreen};
+`;
+
+export const PersonalityContent = styled.Text`
+  display: inline;
+  font-family: ${({ theme }) => theme.fontFamily.nsRegular};
+  font-size: ${({ theme }) => theme.fontSize.base};
+  color: ${({ theme }) => theme.colors.textGray};
+`;
+
+export const ButtonText = styled.Text`
+  font-family: ${({ theme }) => theme.fontFamily.nsRegular};
+  font-size: ${({ theme }) => theme.fontSize.sm};
+  color: ${({ theme }) => theme.colors.black};
 `;
