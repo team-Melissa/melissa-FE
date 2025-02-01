c95b3cb8f3b6a267282eed80d9f0f037f078fe45
NARARIA03 <hyeonseong0305@gmail.com>
2025-02-01 16:14:12 +0900

    MB1-143 Refactor: RootLayout의 Color & SafeView 제거

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,10 @@
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
-import { Slot, usePathname } from "expo-router";
+import { Slot } from "expo-router";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { useReactQueryDevTools } from "@dev-plugins/react-query";
-import { useCallback, useEffect, useState } from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
-import styled, { ThemeProvider } from "styled-components/native";
+import { useEffect, useState } from "react";
+import { ThemeProvider } from "styled-components/native";
 import { theme } from "@/src/constants/theme";
 import queryClient from "@/src/libs/queryClient";
 import initializeApp from "@/src/utils/initializeApp";
@@ -18,19 +17,14 @@ SplashScreen.preventAutoHideAsync();
 function RootLayout() {
   useReactQueryDevTools(queryClient);
   const [isReady, setIsReady] = useState<boolean>(false);
-  const pathname = usePathname();
-
-  console.log(pathname);
-
-  const onLayoutRootView = useCallback(() => {
-    if (isReady) {
-      SplashScreen.hide();
-    }
-  }, [isReady]);
 
   useEffect(() => {
     initializeApp(setIsReady);
   }, []);
+
+  useEffect(() => {
+    if (isReady) SplashScreen.hideAsync();
+  }, [isReady]);
 
   if (!isReady) {
     return null;
@@ -39,24 +33,11 @@ function RootLayout() {
   return (
     <ThemeProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
-        <ColorView pathname={pathname} onLayout={onLayoutRootView}>
-          <SafeView>
-            <StatusBar />
-            <Slot />
-          </SafeView>
-        </ColorView>
+        <StatusBar />
+        <Slot />
       </QueryClientProvider>
     </ThemeProvider>
   );
 }
-
-const ColorView = styled.View<{ pathname: string }>`
-  flex: 1;
-  background-color: ${({ pathname }) => (pathname === "/login" ? "#f0f5f8" : "#ffffff")};
-`;
-
-const SafeView = styled(SafeAreaView)`
-  flex: 1;
-`;
 
 export default RootLayout;
