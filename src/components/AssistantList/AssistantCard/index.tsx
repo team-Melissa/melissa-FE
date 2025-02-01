f416e9355e0b3dc8da095a179c29e8839ee78f39
NARARIA03 <hyeonseong0305@gmail.com>
2025-02-01 21:02:37 +0900

    MB1-143 Feat: aiProfileId가 있는 경우 검색 매개변수로 전달하도록 구현

--- src/components/AssistantList/AssistantCard/index.tsx
+++ src/components/AssistantList/AssistantCard/index.tsx
@@ -16,13 +16,16 @@ function AssistantCard({ item }: Props) {
   const router = useRouter();
   const { mutate: removeMutate } = useRemoveAssistant();
 
-  const routeMakeAssistantPage = () => {
-    router.push("/(app)/make-assistant");
+  const routeMakeAssistantPage = (aiProfileId: number | null = null) => {
+    if (!aiProfileId) {
+      return router.push("/(app)/make-assistant");
+    }
+    return router.push(`/(app)/make-assistant?aiProfileId=${aiProfileId}`);
   };
 
   if ("isGenerateButton" in item) {
     return (
-      <S.ItemBox onPress={routeMakeAssistantPage}>
+      <S.ItemBox onPress={() => routeMakeAssistantPage()}>
         <S.PlusImage source={require("@/assets/images/plus.svg")} />
         <S.GenAiText>새로운 서포터 추가하기</S.GenAiText>
       </S.ItemBox>
@@ -32,7 +35,7 @@ function AssistantCard({ item }: Props) {
   const { aiProfileId, profileName, imageUrl, hashTag1, hashTag2, feature1, feature2, feature3 } =
     item;
 
-  // Todo: 복제/삭제/선택 로직 구현
+  // Todo: 복제 로직 구현
 
   return (
     <TouchableWithoutFeedback>
@@ -41,7 +44,7 @@ function AssistantCard({ item }: Props) {
         <NameTag name={profileName} tag1={hashTag1} tag2={hashTag2} />
         <Personality feat1={feature1} feat2={feature2} feat3={feature3} />
         <S.ButtonBox>
-          <CardButton onPress={() => {}}>복제하기</CardButton>
+          <CardButton onPress={() => routeMakeAssistantPage(aiProfileId)}>복제하기</CardButton>
           <CardButton onPress={() => removeMutate(aiProfileId)}>삭제하기</CardButton>
         </S.ButtonBox>
       </S.ItemBox>
