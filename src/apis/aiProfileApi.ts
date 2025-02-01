3e04dfb4f402342d6a8bbb95e1cbc0b73cad9939
NARARIA03 <hyeonseong0305@gmail.com>
2025-02-01 18:41:14 +0900

    MB1-143 Feat: Ai 리스트를 가져오고, 생성 트리거를 포함시키는 함수 구현

--- src/apis/aiProfileApi.ts
+++ src/apis/aiProfileApi.ts
@@ -1,8 +1,23 @@
 import axiosInstance from "@/src/libs/axiosInstance";
 import endpoint from "@/src/constants/endpoint";
-import { AiProfileMakeAnswers, AiProfileMakeResult } from "@/src/types/aiProfileTypes";
+import {
+  AiProfileListResult,
+  AiProfileListWithGenerateAiTrigger,
+  AiProfileMakeAnswers,
+  AiProfileMakeResult,
+} from "@/src/types/aiProfileTypes";
 
 export const makeAssistantFn = async (answers: AiProfileMakeAnswers) => {
   const { data } = await axiosInstance.post<AiProfileMakeResult>(endpoint.aiProfile, answers);
   return data;
+};
+
+export const assistantListFn = async () => {
+  const { data } = await axiosInstance.get<AiProfileListResult>(endpoint.aiProfile);
+  const dataWithGenerateAiTrigger: AiProfileListWithGenerateAiTrigger[] = [
+    ...data.result,
+    { isGenerateButton: true },
+  ];
+  console.log(dataWithGenerateAiTrigger);
+  return dataWithGenerateAiTrigger;
 };
