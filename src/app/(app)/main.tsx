7134722bff7a8f30c3a98be1766a491a5b8b449b
NARARIA03 <hyeonseong0305@gmail.com>
2025-02-02 20:47:40 +0900

    MB1-141 Refactor: 초기 유저 설정 기본값 mutation을 app라우터로 이동

--- src/app/(app)/main.tsx
+++ src/app/(app)/main.tsx
@@ -1,24 +1,41 @@
-import { useState } from "react";
-import AssistantList from "@/src/components/AssistantList";
-import Button from "@/src/components/ui/Button";
+import { useEffect } from "react";
+import { useIsNewUserContext } from "@/src/contexts/IsNewUserProvider";
+import useRegisterSetting from "@/src/hooks/useRegisterSetting";
+import CommonError from "@/src/components/ui/CommonError";
+import Loading from "@/src/components/ui/Loading";
 import CalendarPage from "@/src/pages/Calendar";
-import { View } from "react-native";
 
 /**
  * @description 달력 나오는 페이지의 라우터
  */
 function MainRouter() {
-  const [isVisible, setIsVisible] = useState<boolean>(false);
+  const isNewUser = useIsNewUserContext();
+  const { isPending, isError, mutate } = useRegisterSetting();
 
-  const handleVisible = () => {
-    setIsVisible((prev) => !prev);
-  };
-  return (
-    // <View style={{ position: "relative", flex: 1, justifyContent: "center", alignItems: "center" }}>
-    // <AssistantList isVisible={isVisible} setIsVisible={setIsVisible} />
-    <CalendarPage />
-    // <Button onPress={handleVisible}>눌러보셈</Button>
-    // </View>
-  );
+  // 초기 유저의 기본 설정값 등록
+  useEffect(() => {
+    if (isNewUser) {
+      mutate();
+    }
+  }, [isNewUser, mutate]);
+
+  // 초기 유저 기본 설정값 등록 로딩 / 에러 관리
+  if (isNewUser) {
+    if (isPending) {
+      return <Loading />;
+    }
+
+    if (isError) {
+      return (
+        <CommonError
+          titleText="앱 초기 로딩 중 에러가 발생했어요."
+          buttonText="다시 로딩하기"
+          onPress={() => mutate()}
+        />
+      );
+    }
+  }
+
+  return <CalendarPage />;
 }
 export default MainRouter;
--- src/pages/Calendar/index.tsx
+++ src/pages/Calendar/index.tsx
@@ -1,13 +1,9 @@
-import { useEffect } from "react";
 import { CalendarList, DateData, LocaleConfig } from "react-native-calendars";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
-import { useIsNewUserContext } from "@/src/contexts/IsNewUserProvider";
-import useRegisterSetting from "@/src/hooks/useRegisterSetting";
-import Loading from "@/src/components/ui/Loading";
-import CommonError from "@/src/components/ui/CommonError";
 import DayComponent from "./DayComponent";
 import { theme } from "@/src/constants/theme";
 import * as S from "./styles";
+import { getCalendarFn } from "@/src/apis/calendarApi";
 
 LocaleConfig.locales["ko"] = {
   monthNames: [
@@ -32,9 +28,6 @@ LocaleConfig.locales["ko"] = {
 LocaleConfig.defaultLocale = "ko";
 
 function CalendarPage(): JSX.Element {
-  const isNewUser = useIsNewUserContext();
-  const { isPending, isError, mutate } = useRegisterSetting();
-
   const handleCopyPress = () => {
     console.log("copy button");
   };
@@ -46,28 +39,6 @@ function CalendarPage(): JSX.Element {
   const handleDayPress = (day: DateData) => {
     console.log("selected day", day);
   };
-
-  useEffect(() => {
-    if (isNewUser) {
-      mutate();
-    }
-  }, [isNewUser, mutate]);
-
-  if (isNewUser) {
-    if (isPending) {
-      return <Loading />;
-    }
-
-    if (isError) {
-      return (
-        <CommonError
-          titleText="앱 초기 로딩 중 에러가 발생했어요."
-          buttonText="다시 로딩하기"
-          onPress={() => mutate()}
-        />
-      );
-    }
-  }
 
   return (
     <S.SafeView>
