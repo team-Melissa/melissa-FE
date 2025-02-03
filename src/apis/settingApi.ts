cb9cec728076e4c8e3302d8d9f072d371e8ec25b
NARARIA03 <hyeonseong0305@gmail.com>
2025-02-03 17:10:34 +0900

    MB1-147 Feat: 유저 설정 가져오는 queryFn, 타입 구현

--- src/apis/settingApi.ts
+++ src/apis/settingApi.ts
@@ -1,6 +1,6 @@
 import endpoint from "@/src/constants/endpoint";
 import axiosInstance from "@/src/libs/axiosInstance";
-import { CheckNewUserType, RegisterSettingType } from "@/src/types/settingTypes";
+import { CheckNewUserType, RegisterSettingType, UserSettingResult } from "@/src/types/settingTypes";
 
 export const registerSettingFn = async () => {
   const { data } = await axiosInstance.post<RegisterSettingType>(endpoint.setting.register);
@@ -9,6 +9,12 @@ export const registerSettingFn = async () => {
 
 export const checkNewUserFn = async () => {
   const { data } = await axiosInstance.get<CheckNewUserType>(endpoint.setting.checkNew);
+  console.log(data);
+  return data;
+};
+
+export const getUserSettingFn = async () => {
+  const { data } = await axiosInstance.get<UserSettingResult>(endpoint.setting.getSetting);
   console.log(data);
   return data;
 };
