c28a6eaefb49954cee5cbe7642beae18d758f922
NARARIA03 <hyeonseong0305@gmail.com>
2025-02-03 23:43:36 +0900

    MB1-148 Refactor: 토스트 메시지 텍스트 중앙집중화

---
+++ src/constants/toastMessage.ts
@@ -0,0 +1,27 @@
+const toastMessage = {
+  fontLoading: {
+    failed: "앱 초기 로딩에 실패했어요. 잠시 후 다시 시도해주세요.",
+  },
+  login: {
+    success: "로그인에 성공했습니다.",
+    failed: "로그인에 실패했습니다.",
+  },
+  logout: {
+    success: "로그아웃에 성공했습니다.",
+    failed: "로그아웃에 실패했습니다.",
+  },
+  tokenExpired: "세션이 만료되었습니다. 다시 로그인 해주세요.",
+  registerSetting: {
+    failed: "앱 초기 로딩 중 에러가 발생했어요.",
+  },
+  updateSetting: {
+    success: "설정 업데이트 완료.",
+    failed: "설정 업데이트 도중 문제 발생.",
+  },
+  removeAssistant: {
+    success: "서포터가 삭제되었습니다.",
+    failed: "서포터 삭제에 실패했습니다. 다시 시도해주세요.",
+  },
+};
+
+export default toastMessage;
