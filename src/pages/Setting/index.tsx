7896b7846799f9056667486f4bc5c4c3b6c0f102
NARARIA03 <hyeonseong0305@gmail.com>
2025-02-03 20:40:34 +0900

    MB1-147 Feat: DateTimePicker 활용해 자는 시간, 알림 시간 설정 팝업 구현

--- src/pages/Setting/index.tsx
+++ src/pages/Setting/index.tsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import { ScrollView, Switch } from "react-native";
 import { useRouter } from "expo-router";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 import Loading from "@/src/components/ui/Loading";
 import useUpdateSetting from "@/src/hooks/useUpdateSetting";
 import useLogout from "@/src/hooks/useLogout";
@@ -15,9 +17,40 @@ interface Props {
 
 function SettingPage({ data }: Props): JSX.Element {
   const { sleepTime, notificationSummary, notificationTime } = data.result;
+
+  const [isDatePickerVisible, setIsDatePickerVisible] = useState<boolean>(false);
+  const [datePickerType, setDatePickerType] = useState<"sleepTime" | "notificationTime" | null>(
+    null
+  );
+
   const router = useRouter();
   const { mutate: settingMutate } = useUpdateSetting(data);
   const { isPending, mutate: logoutMutate } = useLogout();
+
+  const showDatePicker = (datePickerType: "sleepTime" | "notificationTime") => {
+    setIsDatePickerVisible(true);
+    setDatePickerType(datePickerType);
+  };
+
+  const hideDatePicker = () => {
+    setIsDatePickerVisible(false);
+    setDatePickerType(null);
+  };
+
+  const handleConfirm = (date: Date) => {
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    console.log(hours, minutes);
+
+    if (datePickerType) {
+      settingMutate({
+        ...data.result,
+        [datePickerType]: `${hours}:${minutes}:00`,
+      });
+    }
+
+    hideDatePicker();
+  };
 
   const handlePrevButton = () => {
     router.back();
@@ -51,13 +84,14 @@ function SettingPage({ data }: Props): JSX.Element {
       </S.TitleBox>
       <ScrollView showsVerticalScrollIndicator={false}>
         <S.SettingBox>
-          <S.ItemButton hitSlop={10}>
+          <S.ItemButton hitSlop={10} onPress={() => showDatePicker("sleepTime")}>
             <S.ItemTitleBox>
               <S.ItemTitleText>자는 시간 설정</S.ItemTitleText>
               <S.ItemDescriptionText>해당 시간에 대화를 초기화 해드려요</S.ItemDescriptionText>
             </S.ItemTitleBox>
-            <S.ItemValueText>{sleepTime}</S.ItemValueText>
+            <S.ItemValueText>{sleepTime.slice(0, -3)}</S.ItemValueText>
           </S.ItemButton>
+
           <S.ItemButton disabled={true}>
             <S.ItemTitleBox>
               <S.ItemTitleText>푸시 알림</S.ItemTitleText>
@@ -65,14 +99,16 @@ function SettingPage({ data }: Props): JSX.Element {
             </S.ItemTitleBox>
             <Switch value={notificationSummary} onChange={handleNotificationSummary} />
           </S.ItemButton>
-          <S.ItemButton hitSlop={10}>
+
+          <S.ItemButton hitSlop={10} onPress={() => showDatePicker("notificationTime")}>
             <S.ItemTitleBox>
               <S.ItemTitleText>알림 시간 설정</S.ItemTitleText>
               <S.ItemDescriptionText>원하는 시간에 대화할 수 있도록</S.ItemDescriptionText>
               <S.ItemDescriptionText>앱 푸시 알림을 보내드려요</S.ItemDescriptionText>
             </S.ItemTitleBox>
-            <S.ItemValueText>{notificationTime}</S.ItemValueText>
+            <S.ItemValueText>{notificationTime.slice(0, -3)}</S.ItemValueText>
           </S.ItemButton>
+
           <S.ItemButton hitSlop={10}>
             <S.ItemTitleBox>
               <S.ItemTitleText>의견 보내기</S.ItemTitleText>
@@ -80,6 +116,7 @@ function SettingPage({ data }: Props): JSX.Element {
               <S.ItemDescriptionText>전달해주세요</S.ItemDescriptionText>
             </S.ItemTitleBox>
           </S.ItemButton>
+
           <S.ItemButton hitSlop={10}>
             <S.ItemTitleBox>
               <S.ItemTitleText>후원하기</S.ItemTitleText>
@@ -87,6 +124,7 @@ function SettingPage({ data }: Props): JSX.Element {
               <S.ItemDescriptionText>큰 도움이 됩니다</S.ItemDescriptionText>
             </S.ItemTitleBox>
           </S.ItemButton>
+
           <S.ItemButton hitSlop={10} onPress={handleLogout}>
             <S.ItemTitleBox>
               <S.ItemTitleText>로그아웃</S.ItemTitleText>
@@ -94,6 +132,13 @@ function SettingPage({ data }: Props): JSX.Element {
           </S.ItemButton>
         </S.SettingBox>
       </ScrollView>
+
+      <DateTimePickerModal
+        isVisible={isDatePickerVisible}
+        mode="time"
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
+      />
     </S.SafeView>
   );
 }
