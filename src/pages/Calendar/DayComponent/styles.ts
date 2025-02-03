3419ffa9d6910728d2e1a19d155e6de4921139ed
NARARIA03 <hyeonseong0305@gmail.com>
2025-02-04 05:37:41 +0900

    MB1-145 Fix: 세로 길이가 다른 기기보다 짧으면 캘린더 세로가 너무 긴 문제 해결

--- src/pages/Calendar/DayComponent/styles.ts
+++ src/pages/Calendar/DayComponent/styles.ts
@@ -1,15 +1,15 @@
 import styled from "styled-components/native";
 import { Image as Img } from "react-native";
-import responsiveToPx from "@/src/utils/responsiveToPx";
+import responsiveToPx, { responsiveToPxByHeight } from "@/src/utils/responsiveToPx";
 
 export const DayBox = styled.TouchableOpacity`
-  width: ${responsiveToPx("54px")};
-  height: ${responsiveToPx("96px")};
+  width: ${responsiveToPxByHeight("54px")};
+  height: ${responsiveToPxByHeight("96px")};
 `;
 
 export const ImageBox = styled.View`
-  width: ${responsiveToPx("54px")};
-  height: ${responsiveToPx("54px")};
+  width: ${responsiveToPxByHeight("54px")};
+  height: ${responsiveToPxByHeight("54px")};
   border-radius: ${({ theme }) => theme.borderRadius.sm};
   justify-content: center;
   align-items: center;
@@ -17,15 +17,15 @@ export const ImageBox = styled.View`
 `;
 
 export const Image = styled(Img)`
-  width: ${responsiveToPx("54px")};
-  height: ${responsiveToPx("54px")};
+  width: ${responsiveToPxByHeight("54px")};
+  height: ${responsiveToPxByHeight("54px")};
 `;
 
 export const TagBox = styled.View`
   background-color: ${({ theme }) => theme.colors.skyBlue};
-  width: ${responsiveToPx("54px")};
-  height: ${responsiveToPx("15px")};
-  margin-top: ${responsiveToPx("6px")};
+  width: ${responsiveToPxByHeight("54px")};
+  height: ${responsiveToPxByHeight("15px")};
+  margin-top: ${responsiveToPxByHeight("3px")};
   border-radius: ${({ theme }) => theme.borderRadius.xs};
   justify-content: center;
   padding-left: ${responsiveToPx("4px")};
