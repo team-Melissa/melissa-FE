2035a0a4664a701f1e2694e6dd6caedb1a808c49
NARARIA03 <hyeonseong0305@gmail.com>
2025-02-03 23:32:23 +0900

    MB1-148 Feat: 초기 유저 설정 등록 실패 시 토스트 메시지 출력

--- src/hooks/useRegisterSetting.ts
+++ src/hooks/useRegisterSetting.ts
@@ -1,4 +1,5 @@
 import { registerSettingFn } from "@/src/apis/settingApi";
+import showToast from "@/src/libs/showToast";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 const useRegisterSetting = () => {
@@ -12,6 +13,7 @@ const useRegisterSetting = () => {
     },
     onError: (error) => {
       console.error(error.response?.data);
+      showToast("앱 초기 로딩 중 에러가 발생했어요.", "error");
     },
   });
 };
