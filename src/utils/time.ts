3cc8421317bb6e6c0ee765477d0a3c2c9b9fa075
NARARIA03 <hyeonseong0305@gmail.com>
2025-02-05 03:31:56 +0900

    MB1-144 Feat: 스레드 요청시간/만료시간 계산 함수 구현

---
+++ src/utils/time.ts
@@ -0,0 +1,36 @@
+type ThreadDate = {
+  year: number;
+  month: number;
+  day: number;
+};
+
+type ExpiredDate = Date;
+
+export const threadDateExpired = (sleepHour: number): [ThreadDate, ExpiredDate] => {
+  const date = new Date();
+  const curHour = date.getHours();
+
+  const createdDate = new Date();
+  const expiredDate = new Date();
+
+  if (sleepHour <= curHour) {
+    // 자는 시간 <= 생성 시간 : 스레드 생성은 당일, 만료일은 다음날 자는 시간
+    expiredDate.setDate(expiredDate.getDate() + 1);
+  } else {
+    // 자는 시간 > 생성 시간: 스레드 생성은 전날, 만료일은 오늘 자는 시간
+    createdDate.setDate(createdDate.getDate() - 1);
+  }
+
+  expiredDate.setHours(sleepHour, 0, 0, 0); // 분 초는 0으로
+
+  return [
+    {
+      year: createdDate.getFullYear(),
+      month: createdDate.getMonth() + 1,
+      day: createdDate.getDate(),
+    },
+    expiredDate,
+  ];
+};
+
+// 만료일 이후면 당일 스레드 재생성
