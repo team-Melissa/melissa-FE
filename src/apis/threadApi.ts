fa80b47ea7d4d3f620e7794832b4656ec83cfd97
NARARIA03 <hyeonseong0305@gmail.com>
2025-02-05 05:31:16 +0900

    MB1-144 Feat: 채팅 목록 가져오는 queryFn, useQuery 구현/예외처리

--- src/apis/threadApi.ts
+++ src/apis/threadApi.ts
@@ -1,17 +1,23 @@
 import axiosInstance from "@/src/libs/axiosInstance";
 import endpoint from "@/src/constants/endpoint";
-import { NewThreadResult } from "@/src/types/threadTypes";
+import { NewThreadResult, ThreadDate } from "@/src/types/threadTypes";
 
-type NewThreadFnType = {
-  aiProfileId: number;
-  year: number;
-  month: number;
-  day: number;
-};
-
-export const newThreadFn = async ({ aiProfileId, year, month, day }: NewThreadFnType) => {
+export const newThreadFn = async ({
+  aiProfileId,
+  year,
+  month,
+  day,
+}: ThreadDate & { aiProfileId: number }) => {
   const { data } = await axiosInstance.post<NewThreadResult>(
     `${endpoint.thread.chat}?aiProfileId=${aiProfileId}&year=${year}&month=${month}&day=${day}`
   );
+  return data;
+};
+
+export const getMessagesFn = async ({ year, month, day }: ThreadDate) => {
+  const { data } = await axiosInstance.get(
+    `${endpoint.thread.chat}?year=${year}&month=${month}&day=${day}`
+  );
+  console.log(data);
   return data;
 };
--- src/pages/Chatting/index.tsx
+++ src/pages/Chatting/index.tsx
@@ -1,8 +1,12 @@
 import { useRouter } from "expo-router";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { Text, TouchableOpacity } from "react-native";
-import * as S from "./styles";
+import { useQuery } from "@tanstack/react-query";
+import { getMessagesFn } from "@/src/apis/threadApi";
+import Loading from "@/src/components/ui/Loading";
+import CommonError from "@/src/components/ui/CommonError";
 import { ThreadDate } from "@/src/types/threadTypes";
+import * as S from "./styles";
 
 interface Props {
   threadDate: ThreadDate;
@@ -12,9 +16,28 @@ interface Props {
 function ChattingPage({ threadDate, expiredDate }: Props): JSX.Element {
   const router = useRouter();
 
+  const { isPending, isError, data, refetch } = useQuery({
+    queryFn: () => getMessagesFn(threadDate),
+    queryKey: ["message", threadDate],
+  });
+
   const handleBackPress = () => {
     router.back();
   };
+
+  if (isPending) {
+    return <Loading />;
+  }
+
+  if (isError) {
+    return (
+      <CommonError
+        titleText="채팅 내역을 불러오지 못했어요"
+        buttonText="다시 불러오기"
+        onPress={refetch}
+      />
+    );
+  }
 
   return (
     <S.SafeView>
