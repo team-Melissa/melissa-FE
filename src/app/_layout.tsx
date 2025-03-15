7313d233fe8ace23d927719bf338491c6adeb584
NARARIA03 <hyeonseong0305@gmail.com>
2025-03-15 12:12:01 +0900

    Chore: 앱 root에 sentry 연결
    (#31)

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,8 @@
 import * as SplashScreen from "expo-splash-screen";
+import * as Sentry from "@sentry/react-native";
 import { StatusBar } from "expo-status-bar";
-import { Slot } from "expo-router";
+import { isRunningInExpoGo } from "expo";
+import { Slot, useNavigationContainerRef } from "expo-router";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { useReactQueryDevTools } from "@dev-plugins/react-query";
 import { useEffect, useState } from "react";
@@ -12,12 +14,34 @@ import initializeApp from "@/src/utils/initializeApp";
 
 SplashScreen.preventAutoHideAsync();
 
+const navigationIntegration = Sentry.reactNavigationIntegration({
+  enableTimeToInitialDisplay: !isRunningInExpoGo(),
+});
+
+Sentry.init({
+  dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
+  debug: __DEV__ ? true : false,
+  tracesSampleRate: __DEV__ ? 1.0 : 0.3,
+  integrations: [
+    // Pass integration
+    navigationIntegration,
+  ],
+  enableNativeFramesTracking: !isRunningInExpoGo(), // Tracks slow and frozen frames in the application
+});
+
 /**
  * @description 폰트 로딩, 라이브러리 provider, 공용 스타일 컴포넌트로 감싸는 레이아웃
  */
 function RootLayout() {
   useReactQueryDevTools(queryClient);
+  const ref = useNavigationContainerRef();
   const [isReady, setIsReady] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (ref?.current) {
+      navigationIntegration.registerNavigationContainer(ref);
+    }
+  }, [ref]);
 
   useEffect(() => {
     initializeApp(setIsReady);
@@ -42,4 +66,4 @@ function RootLayout() {
   );
 }
 
-export default RootLayout;
+export default Sentry.wrap(RootLayout);
