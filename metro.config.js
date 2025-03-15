162333eba544b928d30c9c6bebb5df7491770678
NARARIA03 <hyeonseong0305@gmail.com>
2025-03-15 11:51:56 +0900

    Chore: metro 번들러에 sentry 연결
    (#31)

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,7 @@
-const { getDefaultConfig } = require("@expo/metro-config");
+const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 const path = require("path");
 
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+const config = getSentryExpoConfig(__dirname);
 
 config.resolver.resolveRequest = (context, moduleName, platform) => {
   if (moduleName.startsWith("es-toolkit")) {
