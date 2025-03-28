e131bcf61cdeefad636f675814d30c1166a1e620
NARARIA03 <hyeonseong0305@gmail.com>
2025-03-29 00:15:54 +0900

    Chore: 핫픽스 재배포를 위한 버전업

--- app.config.ts
+++ app.config.ts
@@ -5,7 +5,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   name: "Melissa",
   slug: "melissa",
   owner: "teammelissa7",
-  version: "1.0.0",
+  version: "1.0.1",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: "myapp",
