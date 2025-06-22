5f7f82f30849fabaf401ba7e0023b75d14052b2a
NARARIA03 <hyeonseong0305@gmail.com>
2025-06-22 16:53:29 +0900

    Chore: AI 프로필 생성 페이지 Intro 컴포넌트 revert (#106)

--- src/features/makeAssistant/containers/MakeAssistantContainer.tsx
+++ src/features/makeAssistant/containers/MakeAssistantContainer.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { questions } from "../constants/questions";
+import Intro from "../components/Intro";
 import Question from "../components/Question";
 import Submit from "../components/Submit";
 import type { TAssistantMakeQnA } from "../types/makeAssistantTypes";
@@ -9,6 +10,7 @@ type MakeAssistantContainerProps = {
 };
 
 export default function MakeAssistantContainer({ prevAnswer }: MakeAssistantContainerProps) {
+  const [isIntro, setIsIntro] = useState<boolean>(true);
   const [cursor, setCursor] = useState<number>(0);
   const [answers, setAnswers] = useState<string[]>(() => {
     if (prevAnswer) {
@@ -19,8 +21,14 @@ export default function MakeAssistantContainer({ prevAnswer }: MakeAssistantCont
   });
 
   useEffect(() => {
-    console.log(answers);
-  }, [answers]);
+    const timer = setTimeout(() => {
+      setIsIntro(false);
+    }, 4000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isIntro) return <Intro />;
 
   if (cursor + 1 <= questions.length) {
     return <Question answer={answers} setAnswer={setAnswers} cursor={cursor} setCursor={setCursor} />;
