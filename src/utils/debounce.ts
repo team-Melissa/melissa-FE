97dfa4cb5cf67d256991a2e56e111bf0b9ba2817
NARARIA03 <hyeonseong0305@gmail.com>
2025-07-22 22:08:22 +0900

    Fix: 디바운스 함수 파라미터/타입 개선 (#111)

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,17 +1,18 @@
-type Fn = (...args: any[]) => void;
+type Fn = (...args: any[]) => any;
+type ReturnFn<F extends Fn> = (...args: Parameters<F>) => ReturnType<F> | void;
 
 /**
  * @description leading 방식의 debounce함수입니다.
  *
  * 첫 실행 이후 delay 동안 실행을 막는 역할입니다.
  */
-export const debounce = <F extends Fn>(callbackFn: F, delay: number = 500) => {
+export const debounce = <F extends Fn>(callbackFn: F, delay: number = 500): ReturnFn<F> => {
   let timer: NodeJS.Timeout | null = null;
   let called: boolean = false;
 
-  return () => {
+  return (...args: Parameters<F>) => {
     if (!called) {
-      callbackFn();
+      callbackFn(...args);
       called = true;
     }
     if (timer) clearTimeout(timer);
