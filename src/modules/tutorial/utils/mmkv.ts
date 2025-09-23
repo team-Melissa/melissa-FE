95401c35fc4d7d2e0ba85bc6636929d044d32d2f
NARARIA03 <hyeonseong0305@gmail.com>
2025-09-24 00:13:09 +0900

    Refactor: mmkv 함수명과 반환타입 개선 (#149)

--- src/modules/tutorial/utils/mmkv.ts
+++ src/modules/tutorial/utils/mmkv.ts
@@ -3,26 +3,26 @@ import { CHAT_TUTORIAL_KEY, DIARY_TUTORIAL_KEY, MAIN_TUTORIAL_KEY, TUTORIAL_MMKV
 
 const storage = new MMKV({ id: TUTORIAL_MMKV_ID });
 
-export const isMainTutorialDone = () => {
-  return storage.getBoolean(MAIN_TUTORIAL_KEY);
+export const getIsMainTutorialOpen = () => {
+  return storage.getBoolean(MAIN_TUTORIAL_KEY) ?? true;
+};
+
+export const getIsChatTutorialOpen = () => {
+  return storage.getBoolean(CHAT_TUTORIAL_KEY) ?? true;
+};
+
+export const getIsDiaryTutorialOpen = () => {
+  return storage.getBoolean(DIARY_TUTORIAL_KEY) ?? true;
 };
 
 export const completeMainTutorial = () => {
-  return storage.set(MAIN_TUTORIAL_KEY, true);
-};
-
-export const isChatTutorialDone = () => {
-  return storage.getBoolean(CHAT_TUTORIAL_KEY);
+  return storage.set(MAIN_TUTORIAL_KEY, false);
 };
 
 export const completeChatTutorial = () => {
-  return storage.set(CHAT_TUTORIAL_KEY, true);
-};
-
-export const isDiaryTutorialDone = () => {
-  return storage.getBoolean(DIARY_TUTORIAL_KEY);
+  return storage.set(CHAT_TUTORIAL_KEY, false);
 };
 
 export const completeDiaryTutorial = () => {
-  return storage.set(DIARY_TUTORIAL_KEY, true);
+  return storage.set(DIARY_TUTORIAL_KEY, false);
 };
