c3e2d0ed1bb8c7b3f91adee3d06e3c1275a03119
NARARIA03 <hyeonseong0305@gmail.com>
2025-11-02 01:35:14 +0900

    Fix: IOS에서 모달 위에 키보드가 열리면, 키보드가 모달을 가리는 문제 대응 (#155)

--- src/modules/modal/components/ModalRoot.tsx
+++ src/modules/modal/components/ModalRoot.tsx
@@ -1,4 +1,11 @@
-import { type GestureResponderEvent, Keyboard, Modal, TouchableWithoutFeedback } from "react-native";
+import {
+  type GestureResponderEvent,
+  Keyboard,
+  KeyboardAvoidingView,
+  Modal,
+  Platform,
+  TouchableWithoutFeedback,
+} from "react-native";
 import type { ReactNode } from "react";
 import styled from "styled-components/native";
 import { useIsKeyboardOpen } from "@/src/hooks/useIsKeyboardOpen";
@@ -20,6 +27,22 @@ export const ModalRoot = ({ isOpen, onClose, children }: ModalRootProps) => {
   const handleInnerClick = (e: GestureResponderEvent) => {
     e.stopPropagation();
   };
+
+  // * IOS의 경우, 모달이 열린 상태에서 키보드가 노출되면, 키보드가 모달을 가리는 문제가 존재합니다.
+  // * KeyboardAvoidingView를 활용해 키보드가 노출되면, 모달이 키보드 위로 올라가도록 처리합니다.
+  if (Platform.OS === "ios") {
+    return (
+      <Modal visible={isOpen} animationType="fade" onRequestClose={onClose} transparent>
+        <KeyboardAvoidingView behavior="padding" style={{ flex: 1 }}>
+          <TouchableWithoutFeedback onPress={handleBackdropPress}>
+            <Backdrop>
+              <TouchableWithoutFeedback onPress={handleInnerClick}>{children}</TouchableWithoutFeedback>
+            </Backdrop>
+          </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
+      </Modal>
+    );
+  }
 
   return (
     <Modal visible={isOpen} animationType="fade" onRequestClose={onClose} transparent statusBarTranslucent>
