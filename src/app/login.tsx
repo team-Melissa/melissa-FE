8b35f99f5c6cb27d6df3da252b78c37d365537bd
NARARIA03 <hyeonseong0305@gmail.com>
2025-11-20 00:07:54 +0900

    Chore: 안드로이드 테스트 목적의 코드 제거 (#157)

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -1,18 +1,13 @@
 import styled from "styled-components/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import LoginContainer from "@/src/features/login/containers/LoginContainer";
-import { Text } from "react-native";
-import { getNotificationToken } from "../libs/mmkv";
 
 /**
  * @description 로그인 페이지의 라우터
  */
 export default function LoginRouter() {
-  const token = getNotificationToken();
-
   return (
     <SafeView>
-      <Text style={{ margin: 20, fontSize: 16 }}>{token}</Text>
       <LoginContainer />
     </SafeView>
   );
