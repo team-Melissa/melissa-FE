dd4d2e4df0d8ec6f5ad3f6f80434278235b12030
NARARIA03 <hyeonseong0305@gmail.com>
2025-11-28 20:49:46 +0900

    Feat: 누락된 바텀 탭 바 버튼 커밋 (#163)

---
+++ src/features/home/components/tabBar/SubTabBarItem.tsx
@@ -0,0 +1,41 @@
+import { NavButton } from "@/src/core/Button";
+import { IconChat } from "@/src/icons/IconChat";
+import { IconWave } from "@/src/icons/IconWave";
+import { useRouter } from "expo-router";
+import { Alert } from "react-native";
+import styled from "styled-components/native";
+
+const SubTabBarItem = () => {
+  const router = useRouter();
+
+  const goToTalkingPage = () => {
+    Alert.alert("준비중인 기능입니다.");
+  };
+
+  const goToChattingPage = () => {
+    router.push("/chatting");
+  };
+
+  return (
+    <Wrapper>
+      <NavButton size="small" onPress={goToTalkingPage}>
+        <IconWave />
+      </NavButton>
+      <NavButton size="small" onPress={goToChattingPage}>
+        <IconChat />
+      </NavButton>
+    </Wrapper>
+  );
+};
+
+export default SubTabBarItem;
+
+const Wrapper = styled.View`
+  position: absolute;
+  flex-direction: row;
+  padding: 3px;
+  right: 30px;
+  bottom: 50px;
+  gap: 10px;
+  border-radius: 99px;
+`;
