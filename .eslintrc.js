0b0ac2d9d532fc5c90870b27e00e546beb2bc524
NARARIA03 <hyeonseong0305@gmail.com>
2025-12-03 00:57:18 +0900

    Feat: eslint/prettier 설정, codegen 후 lint 실행, postinstall 스크립트 추가 (#167)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: ["expo", "plugin:@tanstack/query/recommended"],
+  extends: ["expo", "plugin:@tanstack/query/recommended", "prettier"],
   ignorePatterns: ["/dist/*"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "warn",
+  },
 };
