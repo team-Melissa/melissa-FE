4da587ff8823dc41eb1e3d88c179882e877d8e92
NARARIA03 <hyeonseong0305@gmail.com>
2025-12-05 01:29:40 +0900

    Feat: 인트로 lottie 애니메이션 종료되면, 초기 유저 false mutation 실행 후 캘린더로 이동 (#170)

--- src/features/intro/components/IntroAnimationView.tsx
+++ src/features/intro/components/IntroAnimationView.tsx
@@ -1,8 +1,20 @@
 import LottieView from 'lottie-react-native';
 import styled from 'styled-components/native';
 
-const IntroAnimationView = () => {
-  return <StyledLottieView autoPlay renderMode="SOFTWARE" source={require('@/assets/lotties/intro.json')} />;
+type Props = {
+  onFinish: () => void;
+};
+
+const IntroAnimationView = ({ onFinish }: Props) => {
+  return (
+    <StyledLottieView
+      autoPlay
+      loop={false}
+      renderMode="SOFTWARE"
+      source={require('@/assets/lotties/intro.json')}
+      onAnimationFinish={onFinish}
+    />
+  );
 };
 
 export default IntroAnimationView;
--- src/features/intro/containers/IntroContainer.tsx
+++ src/features/intro/containers/IntroContainer.tsx
@@ -1,12 +1,33 @@
+import { getCheckNewUserQueryKey, useCreateDefaultUserSetting } from '@/src/apis/_generated/serverAPI';
 import { COLOR } from '@/src/constants/theme';
+import { useQueryClient } from '@tanstack/react-query';
+import { useRouter } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import styled from 'styled-components/native';
 import IntroAnimationView from '../components/IntroAnimationView';
 
 const IntroContainer = () => {
+  const queryClient = useQueryClient();
+  const router = useRouter();
+
+  const createDefaultUserSettingMutation = useCreateDefaultUserSetting({
+    mutation: {
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: getCheckNewUserQueryKey() });
+        router.replace('/(app)/(tab)/calendar');
+      },
+      onError: () => router.replace('/login'),
+    },
+  });
+
+  const handleIntroFinish = () => {
+    if (createDefaultUserSettingMutation.isPending) return;
+    createDefaultUserSettingMutation.mutate();
+  };
+
   return (
     <Wrapper>
-      <IntroAnimationView />
+      <IntroAnimationView onFinish={handleIntroFinish} />
     </Wrapper>
   );
 };
