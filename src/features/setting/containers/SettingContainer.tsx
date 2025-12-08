466ab33438f0b7a028fc4e3a83bf6a8f39836f59
NARARIA03 <hyeonseong0305@gmail.com>
2025-12-08 21:34:54 +0900

    Chore: react-native-modal-datetime-picker 제거 (#177)

--- src/features/setting/containers/SettingContainer.tsx
+++ src/features/setting/containers/SettingContainer.tsx
@@ -1,17 +1,16 @@
-import { ScrollView } from 'react-native';
-import { useUpdates } from 'expo-updates';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import styled from 'styled-components/native';
-import DateTimePickerModal from 'react-native-modal-datetime-picker';
 import Loading from '@/src/components/ui/Loading';
 import responsiveToPx from '@/src/utils/responsiveToPx';
+import { useUpdates } from 'expo-updates';
+import { ScrollView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import styled from 'styled-components/native';
+import DeleteAccountItem from '../components/DeleteAccountItem';
+import LogoutItem from '../components/LogoutItem';
+import NotiTimeSettingItem from '../components/NotiTimeSettingItem';
+import PushNotiSettingItem from '../components/PushNotiSettingItem';
+import SendMailItem from '../components/SendMailItem';
 import SettingHeader from '../components/SettingHeader';
 import SleepTimeSettingItem from '../components/SleepTimeSettingItem';
-import PushNotiSettingItem from '../components/PushNotiSettingItem';
-import NotiTimeSettingItem from '../components/NotiTimeSettingItem';
-import SendMailItem from '../components/SendMailItem';
-import LogoutItem from '../components/LogoutItem';
-import DeleteAccountItem from '../components/DeleteAccountItem';
 import { useSetting } from '../hooks/useSetting';
 import type { UserSettingDTO } from '../types/settingTypes';
 
@@ -53,12 +52,12 @@ export default function SettingContainer({ data }: SettingContainerProps) {
         </SettingBox>
         {currentlyRunning.updateId && <OTAVersionTxt>OTA version: {currentlyRunning.updateId}</OTAVersionTxt>}
       </ScrollView>
-      <DateTimePickerModal
+      {/* <DateTimePickerModal
         isVisible={isDatePickerVisible}
         mode="time"
         onConfirm={handleConfirm}
         onCancel={hideDatePicker}
-      />
+      /> */}
     </SafeView>
   );
 }
