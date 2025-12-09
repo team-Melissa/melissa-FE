ffa6e7e96710abfb7c12413cddc0aafd39298f29
NARARIA03 <hyeonseong0305@gmail.com>
2025-12-09 23:16:12 +0900

    Fix: 오타 수정, 불필요한 래핑함수 제거 (#177)

--- src/features/setting/components/SettingList.tsx
+++ src/features/setting/components/SettingList.tsx
@@ -23,10 +23,6 @@ const SettingList = ({ settingData, onNotificationToggle, onSleepTimeChange, onN
   const [timePickerMode, setTimePickerMode] = useState<TimePickerMode>(null);
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  const handleNotificationToggle = (notificationEnabled: boolean) => {
-    onNotificationToggle(notificationEnabled);
-  };
-
   const handleSleepTimePickerOpen = () => {
     setTimePickerMode('sleepTime');
     onOpen();
@@ -63,7 +59,7 @@ const SettingList = ({ settingData, onNotificationToggle, onSleepTimeChange, onN
   return (
     <Wrapper>
       <SettingItem title="푸시 알림" description="푸시 알림을 허용/차단할 수 있어요.">
-        <Switch checked={settingData.notificationEnabled} onCheckedChange={handleNotificationToggle} />
+        <Switch checked={settingData.notificationEnabled} onCheckedChange={onNotificationToggle} />
       </SettingItem>
       <SettingItem title="일기 자동 작성 시간" description="정리 안된 일기를 이 시간에 작성해드려요.">
         <StyledButton onPress={handleSleepTimePickerOpen} hitSlop={5}>
@@ -71,7 +67,7 @@ const SettingList = ({ settingData, onNotificationToggle, onSleepTimeChange, onN
           <StyledIconArrowRight />
         </StyledButton>
       </SettingItem>
-      <SettingItem title="알람 시간" description={'원하는 시간에 대화할 수 있도록\n앱 푸쉬 알림을 보내드려요.'}>
+      <SettingItem title="알람 시간" description={'원하는 시간에 대화할 수 있도록\n앱 푸시 알림을 보내드려요.'}>
         <StyledButton onPress={handleNotificationTimePickerOpen} hitSlop={5}>
           <MiddleTitle color="title">{settingData.notificationTime}</MiddleTitle>
           <StyledIconArrowRight />
