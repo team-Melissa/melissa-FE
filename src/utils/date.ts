15c6d1b8eac8c3906411ab27a094ed6380fcb8d5
NARARIA03 <hyeonseong0305@gmail.com>
2025-12-13 20:42:28 +0900

    Refactor: 현재 year, month, day를 가져오는 유틸 함수 구조 개선 (#186)

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,3 +1,13 @@
+export const getTodayDate = () => {
+  const now = new Date();
+
+  const year = now.getFullYear();
+  const month = now.getMonth() + 1;
+  const day = now.getDate();
+
+  return { year, month, day };
+};
+
 /**
  * ISO 8601 형식의 날짜 문자열을 year, month, day 형식으로 변환합니다.
  * @param isoDateString - ISO 8601 형식의 날짜 문자열 (Ex: "2025-12-05T11:59:44.351672")
