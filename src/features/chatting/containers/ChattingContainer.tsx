1b109fa222e1da7b4f943952f089c204105ce627
NARARIA03 <hyeonseong0305@gmail.com>
2025-12-14 18:07:52 +0900

    Feat: 채팅 데이터 query API 연동 (#191)

--- src/features/chatting/containers/ChattingContainer.tsx
+++ src/features/chatting/containers/ChattingContainer.tsx
@@ -1,26 +1,38 @@
+import { useGetMessages } from '@/src/apis/_generated/serverAPI';
 import { COLOR } from '@/src/constants/theme';
 import { useRouter } from 'expo-router';
-import { ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import styled from 'styled-components/native';
 import ChattingHeader from '../components/ChattingHeader';
 import ChattingStartBadge from '../components/ChattingStartBadge';
 import { useChattingQueryParams } from '../hooks/useChattingQueryParams';
+import { chatListFilter } from '../utils';
 
 const ChattingContainer = () => {
   const { aiProfileId, year, month, day } = useChattingQueryParams();
   const router = useRouter();
 
+  const { data: chatList } = useGetMessages(
+    { aiProfileId, year, month, day },
+    {
+      query: {
+        select: chatListFilter,
+      },
+    }
+  );
+
   const handleBackClick = () => {
     router.back();
   };
 
+  if (!chatList) return null;
+
   return (
     <SafeView>
       <ChattingHeader characterId={aiProfileId} onBackClick={handleBackClick} />
-      <ScrollView>
+      <StyledScrollView>
         <ChattingStartBadge characterId={aiProfileId} year={year % 100} month={month} day={day} />
-      </ScrollView>
+      </StyledScrollView>
     </SafeView>
   );
 };
@@ -31,4 +43,8 @@ const SafeView = styled(SafeAreaView)`
   flex: 1;
   background-color: ${COLOR.background};
   padding: 0 18px;
+`;
+
+const StyledScrollView = styled.ScrollView`
+  flex: 1;
 `;
