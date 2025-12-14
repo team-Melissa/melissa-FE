6d367595003ff47ae7b9ab6735436bacde1b17fd
NARARIA03 <hyeonseong0305@gmail.com>
2025-12-14 19:11:58 +0900

    Feat: 캐릭터/사용자 채팅 말풍선 컴포넌트 구현 (#191)

---
+++ src/features/chatting/components/UserChatBubble.tsx
@@ -0,0 +1,27 @@
+import { COLOR } from '@/src/constants/theme';
+import { Body1 } from '@/src/core/Txt';
+import responsiveToPx from '@/src/utils/responsiveToPx';
+import styled from 'styled-components/native';
+import type { ChatData } from '../types';
+
+type Props = {
+  chat: ChatData;
+};
+
+const UserChatBubble = ({ chat }: Props) => {
+  return (
+    <Wrapper>
+      <Body1 color="white">{chat.content}</Body1>
+    </Wrapper>
+  );
+};
+
+export default UserChatBubble;
+
+const Wrapper = styled.View`
+  max-width: ${responsiveToPx('250px')};
+  align-self: flex-end;
+  padding: 10px 12px;
+  border-radius: 13px;
+  background-color: ${COLOR.main};
+`;
