2dcbf1ef35aa072b48baf3e159c5124dcdf55d4b
NARARIA03 <hyeonseong0305@gmail.com>
2026-01-01 19:19:20 +0900

    Feat: 피드백 페이지 헤더 구현 (#209)

---
+++ src/features/feedback/components/FeedbackHeader.tsx
@@ -0,0 +1,42 @@
+import { LargeTitle } from '@/src/core/Txt';
+import { IconArrowDown } from '@/src/icons';
+import responsiveToPx from '@/src/utils/responsiveToPx';
+import { TouchableOpacity } from 'react-native';
+import styled from 'styled-components/native';
+
+type Props = {
+  onBackClick: () => void;
+};
+
+const FeedbackHeader = ({ onBackClick }: Props) => {
+  return (
+    <Wrapper>
+      <TouchableOpacity onPress={onBackClick} hitSlop={5}>
+        <StyledIconArrowLeft width={30} height={30} />
+      </TouchableOpacity>
+      <LargeTitle color="title">의견 보내기</LargeTitle>
+      <EmptyView />
+    </Wrapper>
+  );
+};
+
+export default FeedbackHeader;
+
+const Wrapper = styled.View`
+  flex-direction: row;
+  width: 100%;
+  padding: 15px 0;
+  justify-content: space-between;
+  margin-bottom: 50px;
+`;
+
+const StyledIconArrowLeft = styled(IconArrowDown)`
+  width: ${responsiveToPx('30px')};
+  height: ${responsiveToPx('30px')};
+  transform: rotate(90deg);
+`;
+
+const EmptyView = styled.View`
+  width: ${responsiveToPx('30px')};
+  height: ${responsiveToPx('30px')};
+`;
