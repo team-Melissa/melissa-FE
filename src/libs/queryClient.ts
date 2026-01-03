b721bf379a7fb31a38ce7c2edac03363498fd507
NARARIA03 <hyeonseong0305@gmail.com>
2026-01-03 17:17:21 +0900

    Refactor: 불필요한 query client의 error 처리 코드 제거 (#216)

--- src/libs/queryClient.ts
+++ src/libs/queryClient.ts
@@ -1,9 +1,4 @@
-import { MutationCache, QueryCache, QueryClient } from '@tanstack/react-query';
-import { router } from 'expo-router';
-import { toast } from '@/src/modules/toast';
-import toastMessage from '@/src/constants/toastMessage';
-import { removeRefreshToken } from '@/src/libs/secureStorage';
-import { removeAccessToken } from '@/src/libs/mmkv';
+import { QueryClient } from '@tanstack/react-query';
 
 export default new QueryClient({
   defaultOptions: {
@@ -12,37 +7,4 @@ export default new QueryClient({
       retry: 0,
     },
   },
-  queryCache: new QueryCache({
-    onError: async (error) => {
-      console.log('글로벌 query 에러');
-      if (error.response?.data) {
-        console.log(error.response.data);
-        const code = error.response.data.code;
-        if (code === 'AUTH4006') {
-          // 다른 기기에서 이미 삭제된 계정인 경우
-          await removeRefreshToken();
-          removeAccessToken();
-          toast({ message: toastMessage.accountNotFound, options: { type: 'error' } });
-          router.navigate('/login');
-        } else if (code !== '401') {
-          toast({ message: error.response?.data.message, options: { type: 'error' } });
-        }
-      }
-    },
-  }),
-  mutationCache: new MutationCache({
-    onError: async (error) => {
-      console.log('글로벌 mutation 에러');
-      if (error.response?.data) {
-        const code = error.response.data.code;
-        if (code === 'AUTH4006') {
-          // 다른 기기에서 이미 삭제된 계정인 경우
-          await removeRefreshToken();
-          removeAccessToken();
-          toast({ message: toastMessage.accountNotFound, options: { type: 'error' } });
-          router.navigate('/login');
-        }
-      }
-    },
-  }),
 });
