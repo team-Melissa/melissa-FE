b2bc4e671e8636652ed430a1d88d62aee408d619
NARARIA03 <hyeonseong0305@gmail.com>
2026-01-20 00:21:22 +0900

    Feat: 튜토리얼에 사용될 데이터 배열 초안 작성 (#221)

---
+++ src/features/tutorial/constants/index.ts
@@ -0,0 +1,27 @@
+export const TUTORIAL_LIST_DATA = [
+  {
+    title: '대화하기 버튼으로 일기 작성 시작!',
+    description: '오늘 하루는 어땠나요?\n + 버튼을 누르거나, 글쓰기 아이콘을 통해 대화를 시작해보세요!',
+    image: require('@/assets/images/tutorials/calendar.png'),
+  },
+  {
+    title: '당신의 이야기들을 확인해보세요.',
+    description: '날짜를 눌러, 당신의 이야기가 담긴 순간으로 돌아가 보세요.',
+    image: require('@/assets/images/tutorials/bottom-sheet.png'),
+  },
+  {
+    title: '마음에 드는 일기를 저장하거나 공유하세요.',
+    description: '우측 공유 버튼을 클릭해 소중한 기록을 남기거나, 나눌 수 있어요.',
+    image: require('@/assets/images/tutorials/share.png'),
+  },
+  {
+    title: '다양한 파트너와 대화해보세요.',
+    description: '취향 저격 파트너를 찾아보세요.\n각기 다른 매력을 가진 친구들과 대화를 나눌 수 있어요!',
+    image: require('@/assets/images/tutorials/character.png'),
+  },
+  {
+    title: '오늘 하루에 대한 기록을 남겨요.',
+    description: '파트너와 대화하다 보면, 일기 쓰기 버튼이 활성화돼요!\n버튼만 누르면 멋진 일기가 뚝딱 완성돼요.',
+    image: require('@/assets/images/tutorials/chatting.png'),
+  },
+];
