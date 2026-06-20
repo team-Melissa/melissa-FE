79be9d694f7e9f34c423d792d026cf02a0f627a8
NARARIA03 <hyeonseong0305@gmail.com>
2026-06-20 15:53:55 +0900

    Fix: 약관 동의 후 확인 클릭 시 다시 약관 동의 페이지로 이동하는 문제 수정 (#252)

--- src/features/agreement/hooks/useAgreementsSubmitMutation.ts
+++ src/features/agreement/hooks/useAgreementsSubmitMutation.ts
@@ -1,8 +1,4 @@
-import {
-  getGetAgreementScreenQueryKey,
-  getGetAgreementStatusQueryKey,
-  useSubmitAgreements,
-} from '@/src/apis/_generated/serverAPI';
+import { getGetAgreementStatusQueryKey, useSubmitAgreements } from '@/src/apis/_generated/serverAPI';
 import toastMessage from '@/src/constants/toastMessage';
 import { toast } from '@/src/modules/toast';
 import { useQueryClient } from '@tanstack/react-query';
@@ -15,13 +11,25 @@ export const useAgreementsSubmitMutation = () => {
   return useSubmitAgreements({
     mutation: {
       onSuccess: async () => {
-        toast({ message: toastMessage.agreement.success, options: { type: 'success' } });
-        await queryClient.invalidateQueries({ queryKey: getGetAgreementStatusQueryKey() });
-        await queryClient.invalidateQueries({ queryKey: getGetAgreementScreenQueryKey() });
+        toast({
+          message: toastMessage.agreement.success,
+          options: {
+            type: 'success',
+          },
+        });
+        await queryClient.invalidateQueries({
+          queryKey: getGetAgreementStatusQueryKey(),
+          refetchType: 'all',
+        });
         router.replace('/(app)');
       },
       onError: () => {
-        toast({ message: toastMessage.agreement.error, options: { type: 'error' } });
+        toast({
+          message: toastMessage.agreement.error,
+          options: {
+            type: 'error',
+          },
+        });
       },
     },
   });
